96daa5fe0030cdd15dee1315dac5f3b5b0d6aa03
Yeshey <yesheysangpo@hotmail.com>
2023-04-06 15:47:17 +0100

    no progress

--- default.nix
+++ default.nix
@@ -1,8 +1,31 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/5bba55eb6e1c627dc1821c9132c5854e0870e2f1.tar.gz"){}, lib, config, types, ... }:
+{ lib, config, types, pkgs, ... }:
 
+# My post: https://discourse.nixos.org/t/vgpu-unlock-for-nixos-22-11-pin-version-of-nixos-hardware/27012
+
+# pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/5bba55eb6e1c627dc1821c9132c5854e0870e2f1.tar.gz"){}
 #hardware-pinned ? import (fetchTarball "https://github.com/NixOS/nixos-hardware/archive/7da029f26849f8696ac49652312c9171bf9eb170.tar.gz"){}
 
 let
+/*
+  my_config = {
+    inherit config; 
+    pkgs = import (builtins.fetchGit {
+         # Descriptive name to make the store path easier to identify                
+         name = "nix_pkgs2";                                                 
+         url = "https://github.com/NixOS/nixpkgs/";                       
+         ref = "refs/heads/nixpkgs-unstable";                     
+         rev = "fadaef5aedb6b35681248f8c6096083b2efeb284";                                           
+     }) { inherit config; };
+  };*/
+
+  pkgs = import (builtins.fetchGit {
+         # Descriptive name to make the store path easier to identify                
+         name = "nix_pkgs_with_singularity";                                                 
+         url = "https://github.com/NixOS/nixpkgs/";                       
+         ref = "refs/heads/nixpkgs-unstable";                     
+         rev = "fadaef5aedb6b35681248f8c6096083b2efeb284";                                           
+     }) { /*inherit (config) system; inherit config;*/ };
+
   cfg = config.hardware.nvidia.vgpu;
 
   mdevctl = pkgs.callPackage ./mdevctl {};
@@ -142,6 +165,7 @@ in
     ];
     */
 
+
     hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable.overrideAttrs (
       { patches ? [], postUnpack ? "", postPatch ? "", preFixup ? "", ... }@attrs: {
         # Overriding https://github.com/NixOS/nixpkgs/tree/nixos-unstable/pkgs/os-specific/linux/nvidia-x11
@@ -149,204 +173,15 @@ in
       name = "nvidia-x11-${vgpuVersion}-${gridVersion}-${config.boot.kernelPackages.kernel.version}";
       version = "${vgpuVersion}";
 
+      /*
+
       src = pkgs.fetchurl {
-              url = "https://drive.google.com/u/0/uc?id=1dCyUteA2MqJaemRKqqTu5oed5mINu9Bw&export=download&confirm=t&uuid=3ebfe434-c85f-47e5-9980-6e086fc3e9a3&at=ANzk5s5BlvlXo-X9pbxIkmBd6P2o:1680699478854";
+              name = "NVIDIA-Linux-x86_64-460.73.01-grid-vgpu-kvm-v5.run"; # So there can be special characters in the link below: https://github.com/NixOS/nixpkgs/issues/6165#issuecomment-141536009
+              url = "https://drive.google.com/u/0/uc?id=1dCyUteA2MqJaemRKqqTu5oed5mINu9Bw&export=download&confirm=t";
               sha256 = "sha256-C8KM8TwaTYhFx/iYeXTgS9UnNDIbuNtSbGk4UwrRLHE=";
             };
 
-      # https://github.com/NixOS/nix/issues/1528
-      /*
-      src = let 
-        srcPath = 
-          if cfg.vgpuKvmDriver != null then
-            cfg.vgpuKvmDriver
-          else
-            throw "No 'gridDriver' option provided with path to driver";
-
-        derivationName = baseNameOf srcPath;
-        storePath = "/nix/store/zzy4bnrd0zzwsjalhbpvsgzqz43n5xic-${derivationName}"; # this should be ${storeHash}
-      in
-        if builtins.pathExists storePath then
-                storePath 
-              else
-                srcPath;
-                */
-
-      #src = pkgs.requireFile {
-      #  name = "NVIDIA-Linux-x86_64-${gridVersion}-grid.run";
-      #  path = "${config.gridDriver}";
-        #sha256 = "0smvmxalxv7v12m0hvd5nx16jmcc7018s8kac3ycmxam8l0k9mw9";
-      #};
-
-      #patches = patches ++ [
-      #  ./nvidia-vgpu-merge.patch
-      #] ++ lib.optional cfg.unlock.enable
-      #  (pkgs.substituteAll {
-      #    src = ./nvidia-vgpu-unlock.patch;
-      #    vgpu_unlock = vgpu_unlock.src;
-      #  });
-
-      installPhase = ''
-    # Install libGL and friends.
-
-    # since version 391, 32bit libraries are bundled in the 32/ sub-directory
-    if [ "$i686bundled" = "1" ]; then
-        mkdir -p "$lib32/lib"
-        cp -prd 32/*.so.* "$lib32/lib/"
-        if [ -d 32/tls ]; then
-            cp -prd 32/tls "$lib32/lib/"
-        fi
-    fi
-
-    mkdir -p "$out/lib"
-    cp -prd *.so.* "$out/lib/"
-    if [ -d tls ]; then
-        cp -prd tls "$out/lib/"
-    fi
-
-    # Install systemd power management executables
-    if [ -e systemd/nvidia-sleep.sh ]; then
-        mv systemd/nvidia-sleep.sh ./
-    fi
-    if [ -e nvidia-sleep.sh ]; then
-        sed -E 's#(PATH=).*#\1"$PATH"#' nvidia-sleep.sh > nvidia-sleep.sh.fixed
-        install -Dm755 nvidia-sleep.sh.fixed $out/bin/nvidia-sleep.sh
-    fi
-
-    if [ -e systemd/system-sleep/nvidia ]; then
-        mv systemd/system-sleep/nvidia ./
-    fi
-    if [ -e nvidia ]; then
-        sed -E "s#/usr(/bin/nvidia-sleep.sh)#$out\\1#" nvidia > nvidia.fixed
-        install -Dm755 nvidia.fixed $out/lib/systemd/system-sleep/nvidia
-    fi
-
-    for i in $lib32 $out; do
-        rm -f $i/lib/lib{glx,nvidia-wfb}.so.* # handled separately
-        rm -f $i/lib/libnvidia-gtk* # built from source
-        if [ "$useGLVND" = "1" ]; then
-            # Pre-built libglvnd
-            rm $i/lib/lib{GL,GLX,EGL,GLESv1_CM,GLESv2,OpenGL,GLdispatch}.so.*
-        fi
-        # Use ocl-icd instead
-        rm -f $i/lib/libOpenCL.so*
-        # Move VDPAU libraries to their place
-        mkdir $i/lib/vdpau
-        mv $i/lib/libvdpau* $i/lib/vdpau
-
-        # Install ICDs, make absolute paths.
-        # Be careful not to modify any original files because this runs twice.
-
-        # OpenCL
-        sed -E "s#(libnvidia-opencl)#$i/lib/\\1#" nvidia.icd > nvidia.icd.fixed
-        install -Dm644 nvidia.icd.fixed $i/etc/OpenCL/vendors/nvidia.icd
-
-        # Vulkan
-        if [ -e nvidia_icd.json.template ] || [ -e nvidia_icd.json ]; then
-            if [ -e nvidia_icd.json.template ]; then
-                # template patching for version < 435
-                sed "s#__NV_VK_ICD__#$i/lib/libGLX_nvidia.so#" nvidia_icd.json.template > nvidia_icd.json.fixed
-            else
-                sed -E "s#(libGLX_nvidia)#$i/lib/\\1#" nvidia_icd.json > nvidia_icd.json.fixed
-            fi
-
-            if [ "$system" = "i686-linux" ]; then
-                install -Dm644 nvidia_icd.json.fixed $i/share/vulkan/icd.d/nvidia_icd.i686.json
-            else
-                install -Dm644 nvidia_icd.json.fixed $i/share/vulkan/icd.d/nvidia_icd.json
-            fi
-        fi
-
-        if [ -e nvidia_layers.json ]; then
-            sed -E "s#(libGLX_nvidia)#$i/lib/\\1#" nvidia_layers.json > nvidia_layers.json.fixed
-            install -Dm644 nvidia_layers.json.fixed $i/share/vulkan/implicit_layer.d/nvidia_layers.json
-        fi
-
-        # EGL
-        if [ "$useGLVND" = "1" ]; then
-            sed -E "s#(libEGL_nvidia)#$i/lib/\\1#" 10_nvidia.json > 10_nvidia.json.fixed
-            sed -E "s#(libnvidia-egl-wayland)#$i/lib/\\1#" 10_nvidia_wayland.json > 10_nvidia_wayland.json.fixed
-
-            install -Dm644 10_nvidia.json.fixed $i/share/glvnd/egl_vendor.d/nvidia.json
-            install -Dm644 10_nvidia_wayland.json.fixed $i/share/glvnd/egl_vendor.d/nvidia_wayland.json
-        fi
-
-    done
-
-    if [ -n "$bin" ]; then
-        # Install the X drivers.
-        mkdir -p $bin/lib/xorg/modules
-        if [ -f libnvidia-wfb.so ]; then
-            cp -p libnvidia-wfb.* $bin/lib/xorg/modules/
-        fi
-        mkdir -p $bin/lib/xorg/modules/drivers
-        cp -p nvidia_drv.so $bin/lib/xorg/modules/drivers
-        mkdir -p $bin/lib/xorg/modules/extensions
-        cp -p libglx*.so* $bin/lib/xorg/modules/extensions
-
-        # Install the kernel module.
-        mkdir -p $bin/lib/modules/$kernelVersion/misc
-        for i in $(find ./kernel -name '*.ko'); do
-            nuke-refs $i
-            cp $i $bin/lib/modules/$kernelVersion/misc/
-        done
-
-        # Install application profiles.
-        if [ "$useProfiles" = "1" ]; then
-            mkdir -p $bin/share/nvidia
-            cp nvidia-application-profiles-*-rc $bin/share/nvidia/nvidia-application-profiles-rc
-            cp nvidia-application-profiles-*-key-documentation $bin/share/nvidia/nvidia-application-profiles-key-documentation
-        fi
-    fi
-
-    # All libs except GUI-only are installed now, so fixup them.
-    for libname in $(find "$out/lib/" $(test -n "$lib32" && echo "$lib32/lib/") $(test -n "$bin" && echo "$bin/lib/") -name '*.so.*')
-    do
-      # I'm lazy to differentiate needed libs per-library, as the closure is the same.
-      # Unfortunately --shrink-rpath would strip too much.
-      if [[ -n $lib32 && $libname == "$lib32/lib/"* ]]; then
-        patchelf --set-rpath "$lib32/lib:$libPath32" "$libname"
-      else
-        patchelf --set-rpath "$out/lib:$libPath" "$libname"
-      fi
-
-      libname_short=`echo -n "$libname" | sed 's/so\..*/so/'`
-
-      if [[ "$libname" != "$libname_short" ]]; then
-        ln -srnf "$libname" "$libname_short"
-      fi
-
-      if [[ $libname_short =~ libEGL.so || $libname_short =~ libEGL_nvidia.so || $libname_short =~ libGLX.so || $libname_short =~ libGLX_nvidia.so ]]; then
-          major=0
-      else
-          major=1
-      fi
-
-      if [[ "$libname" != "$libname_short.$major" ]]; then
-        ln -srnf "$libname" "$libname_short.$major"
-      fi
-    done
-
-    if [ -n "$bin" ]; then
-        # Install /share files.
-        mkdir -p $bin/share/man/man1
-        cp -p *.1.gz $bin/share/man/man1
-        rm -f $bin/share/man/man1/{nvidia-xconfig,nvidia-settings,nvidia-persistenced}.1.gz
-
-        # Install the programs.
-        for i in nvidia-cuda-mps-control nvidia-cuda-mps-server nvidia-smi nvidia-debugdump; do
-            if [ -e "$i" ]; then
-                install -Dm755 $i $bin/bin/$i
-                # unmodified binary backup for mounting in containers
-                install -Dm755 $i $bin/origBin/$i
-                patchelf --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-                    --set-rpath $out/lib:$libPath $bin/bin/$i
-            fi
-        done
-        # FIXME: needs PATH and other fixes
-        # install -Dm755 nvidia-bug-report.sh $bin/bin/nvidia-bug-report.sh
-    fi
-'';
+      */
 
     });
 
