6b62a0e26c3509f756dbdeda92fbec5d4d932384
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2023-06-27 14:53:04 +0800

    Remove reference to frida
    Frida is no longer necessary with the removal of the userspace ioctl hook

--- flake.nix
+++ flake.nix
@@ -1,12 +1,8 @@
 {
   description = "NixOS module which provides NVIDIA vGPU functionality";
 
-  # Changed to use my fork because of the "Python version mismatch" error.
-  # SO in my fork it uses channel 23.05 instead of unstable. But there should be a better way to approach this.
-  inputs.frida.url = "github:Yeshey/frida-nix";
-
-  outputs = { self, frida }: {
-    nixosModules.nvidia-vgpu = import ./default.nix frida;
+  outputs = { self }: {
+    nixosModules.nvidia-vgpu = import ./default.nix;
   };
 }
 
