527c13727337301373b7b684a29d29ecb1744192
name <email>
2023-11-24 02:46:14 +0100

    fix 28

--- default.nix
+++ default.nix
@@ -11,9 +11,9 @@ let
 in
 let
   # UNCOMMENT this to pin the version of pkgs if this stops working
-  #pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/06278c77b5d162e62df170fec307e83f1812d94b.tar.gz") {
-  #    # config.allowUnfree = true;
-  #};
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/06278c77b5d162e62df170fec307e83f1812d94b.tar.gz") {
+    config.allowUnfree = true;
+  };
 
   cfg = config.hardware.nvidia.vgpu;
 
