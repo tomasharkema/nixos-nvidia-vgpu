9bfbac7ab121213f744271b9ff8ccbbc510549c6
Yeshey <yesheysangpo@hotmail.com>
2024-04-29 13:46:37 +0100

    Update default.nix

--- mdevctl/default.nix
+++ mdevctl/default.nix
@@ -1,4 +1,55 @@
-{ lib, stdenv, fetchFromGitHub, makeWrapper, bash, getopt, jq }:
+{ lib
+, rustPlatform
+, fetchCrate
+, docutils
+, installShellFiles
+, bash
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "mdevctl";
+  version = "1.2.0";
+
+  src = fetchCrate {
+    inherit pname version;
+    hash = "sha256-0X/3DWNDPOgSNNTqcj44sd7DNGFt+uGBjkc876dSgU8=";
+  };
+
+  cargoHash = "sha256-TmumQBWuH5fJOe2qzcDtEGbmCs2G9Gfl8mH7xifzRGc=";
+
+  nativeBuildInputs = [
+    docutils
+    installShellFiles
+  ];
+
+  # had to add this
+  postPatch = ''
+    substituteInPlace 60-mdevctl.rules \
+      --replace /usr/sbin/ $out/ \
+      --replace /bin/sh ${bash}/bin/sh
+  '';
+
+  postInstall = ''
+    ln -s mdevctl $out/bin/lsmdev
+
+    install -Dm444 60-mdevctl.rules -t $out/lib/udev/rules.d
+
+    installManPage $releaseDir/build/mdevctl-*/out/mdevctl.8
+    ln -s mdevctl.8 $out/share/man/man8/lsmdev.8
+
+    installShellCompletion $releaseDir/build/mdevctl-*/out/{lsmdev,mdevctl}.bash
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/mdevctl/mdevctl";
+    description = "A mediated device management utility for linux";
+    license = licenses.lgpl21Only;
+    maintainers = with maintainers; [ edwtjo ];
+    platforms = platforms.linux;
+  };
+}
+
+/* { lib, stdenv, fetchFromGitHub, makeWrapper, bash, getopt, jq }:
 
 stdenv.mkDerivation rec {
   name = "mdevctl";
@@ -43,3 +94,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.linux;
   };
 }
+*/
