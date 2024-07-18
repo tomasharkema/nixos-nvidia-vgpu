def65905e610f4356e422f174741d505038d7e10
Tomas Harkema <tomas@harkema.io>
2024-07-18 22:12:52 +0200

    fix

--- default.nix
+++ default.nix
@@ -1,6 +1,9 @@
-{ pkgs, lib, config, ... }:
-
-let
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
   cfg = config.hardware.nvidia.vgpu;
 
   mdevctl = pkgs.callPackage ./mdevctl {};
@@ -12,33 +15,36 @@ let
   guestVersion = "552.74";
 
   combinedZipName = "NVIDIA-GRID-Linux-KVM-${vgpuVersion}-${gridVersion}-${guestVersion}.zip";
-  requireFile = { name, ... }@args: pkgs.requireFile (rec {
-    inherit name;
-    url = "https://www.nvidia.com/object/vGPU-software-driver.html";
-    message = ''
-      Unfortunately, we cannot download file ${name} automatically.
-      This file can be extracted from ${combinedZipName}.
-      Please go to ${url} to download it yourself, and add it to the Nix store
-      using either
-        nix-store --add-fixed sha256 ${name}
-      or
-        nix-prefetch-url --type sha256 file:///path/to/${name}
+  requireFile = {name, ...} @ args:
+    pkgs.requireFile (rec {
+        inherit name;
+        url = "https://www.nvidia.com/object/vGPU-software-driver.html";
+        message = ''
+          Unfortunately, we cannot download file ${name} automatically.
+          This file can be extracted from ${combinedZipName}.
+          Please go to ${url} to download it yourself, and add it to the Nix store
+          using either
+            nix-store --add-fixed sha256 ${name}
+          or
+            nix-prefetch-url --type sha256 file:///path/to/${name}
+        '';
+      }
+      // args);
+
+  nvidia-vgpu-kvm-src =
+    pkgs.runCommand "nvidia-${vgpuVersion}-vgpu-kvm-src" {
+      src = requireFile {
+        name = "NVIDIA-Linux-x86_64-${vgpuVersion}-vgpu-kvm.run";
+        sha256 = "00ay1f434dbls6p0kaawzc6ziwlp9dnkg114ipg9xx8xi4360zzl";
+      };
+    } ''
+      mkdir $out
+      cd $out
+
+      # From unpackManually() in builder.sh of nvidia-x11 from nixpkgs
+      skip=$(sed 's/^skip=//; t; d' $src)
+      tail -n +$skip $src | xz -d | tar xvf -
     '';
-  } // args);
-
-  nvidia-vgpu-kvm-src = pkgs.runCommand "nvidia-${vgpuVersion}-vgpu-kvm-src" {
-    src = requireFile {
-      name = "NVIDIA-Linux-x86_64-${vgpuVersion}-vgpu-kvm.run";
-      sha256 = "00ay1f434dbls6p0kaawzc6ziwlp9dnkg114ipg9xx8xi4360zzl";
-    };
-  } ''
-    mkdir $out
-    cd $out
-
-    # From unpackManually() in builder.sh of nvidia-x11 from nixpkgs
-    skip=$(sed 's/^skip=//; t; d' $src)
-    tail -n +$skip $src | xz -d | tar xvf -
-  '';
 
   vgpu_unlock = pkgs.stdenv.mkDerivation {
     name = "nvidia-vgpu-unlock";
@@ -51,7 +57,7 @@ let
       sha256 = "0s8bmscb8irj1sggfg1fhacqd1lh59l326bnrk4a2g4qngsbkix3";
     };
 
-    buildInputs = [ (pythonPackages.python.withPackages (p: [ p.frida-python ])) ];
+    buildInputs = [(pythonPackages.python.withPackages (p: [p.frida-python]))];
 
     postPatch = ''
       substituteInPlace vgpu_unlock \
@@ -60,8 +66,7 @@ let
 
     installPhase = "install -Dm755 vgpu_unlock $out/bin/vgpu_unlock";
   };
-in
-{
+in {
   options = {
     hardware.nvidia.vgpu = {
       enable = lib.mkEnableOption "vGPU support";
@@ -74,99 +79,112 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
+  config = lib.mkIf true {
     hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable.overrideAttrs (
-      { patches ? [], postUnpack ? "", postPatch ? "", preFixup ? "", ... }@attrs: {
-      name = "nvidia-x11-${vgpuVersion}-${gridVersion}-${config.boot.kernelPackages.kernel.version}";
-      version = "${vgpuVersion}";
+      {
+        patches ? [],
+        postUnpack ? "",
+        postPatch ? "",
+        preFixup ? "",
+        ...
+      } @ attrs: {
+        name = "nvidia-x11-${vgpuVersion}-${gridVersion}-${config.boot.kernelPackages.kernel.version}";
+        version = "${vgpuVersion}";
 
-      src = requireFile {
-        name = "NVIDIA-Linux-x86_64-${gridVersion}-grid.run";
-        sha256 = "0smvmxalxv7v12m0hvd5nx16jmcc7018s8kac3ycmxam8l0k9mw9";
-      };
+        src = requireFile {
+          name = "NVIDIA-Linux-x86_64-${gridVersion}-grid.run";
+          sha256 = "0smvmxalxv7v12m0hvd5nx16jmcc7018s8kac3ycmxam8l0k9mw9";
+        };
 
-      patches = patches ++ [
-        ./nvidia-vgpu-merge.patch
-      ] ++ lib.optional cfg.unlock.enable
-        (pkgs.substituteAll {
-          src = ./nvidia-vgpu-unlock.patch;
-          vgpu_unlock = vgpu_unlock.src;
-        });
+        patches =
+          patches
+          ++ [
+            ./nvidia-vgpu-merge.patch
+          ]
+          ++ lib.optional cfg.unlock.enable
+          (pkgs.substituteAll {
+            src = ./nvidia-vgpu-unlock.patch;
+            vgpu_unlock = vgpu_unlock.src;
+          });
 
-      postUnpack = postUnpack + ''
-        # More merging, besides patch above
-        cp -r ${nvidia-vgpu-kvm-src}/init-scripts .
-        cp ${nvidia-vgpu-kvm-src}/kernel/common/inc/nv-vgpu-vfio-interface.h kernel/common/inc//nv-vgpu-vfio-interface.h
-        cp ${nvidia-vgpu-kvm-src}/kernel/nvidia/nv-vgpu-vfio-interface.c kernel/nvidia/nv-vgpu-vfio-interface.c
-        echo "NVIDIA_SOURCES += nvidia/nv-vgpu-vfio-interface.c" >> kernel/nvidia/nvidia-sources.Kbuild
-        cp -r ${nvidia-vgpu-kvm-src}/kernel/nvidia-vgpu-vfio kernel/nvidia-vgpu-vfio
+        postUnpack = ''
+          ${postUnpack ? ""}
+          # More merging, besides patch above
+          cp -r ${nvidia-vgpu-kvm-src}/init-scripts .
+          cp ${nvidia-vgpu-kvm-src}/kernel/common/inc/nv-vgpu-vfio-interface.h kernel/common/inc//nv-vgpu-vfio-interface.h
+          cp ${nvidia-vgpu-kvm-src}/kernel/nvidia/nv-vgpu-vfio-interface.c kernel/nvidia/nv-vgpu-vfio-interface.c
+          echo "NVIDIA_SOURCES += nvidia/nv-vgpu-vfio-interface.c" >> kernel/nvidia/nvidia-sources.Kbuild
+          cp -r ${nvidia-vgpu-kvm-src}/kernel/nvidia-vgpu-vfio kernel/nvidia-vgpu-vfio
 
-        for i in libnvidia-vgpu.so.${vgpuVersion} libnvidia-vgxcfg.so.${vgpuVersion} nvidia-vgpu-mgr nvidia-vgpud vgpuConfig.xml sriov-manage; do
-          cp ${nvidia-vgpu-kvm-src}/$i $i
-        done
+          for i in libnvidia-vgpu.so.${vgpuVersion} libnvidia-vgxcfg.so.${vgpuVersion} nvidia-vgpu-mgr nvidia-vgpud vgpuConfig.xml sriov-manage; do
+            cp ${nvidia-vgpu-kvm-src}/$i $i
+          done
 
-        chmod -R u+rw .
-      '';
+          chmod -R u+rw .
+        '';
 
-      postPatch = postPatch + ''
-        # Move path for vgpuConfig.xml into /etc
-        sed -i 's|/usr/share/nvidia/vgpu|/etc/nvidia-vgpu-xxxxx|' nvidia-vgpud
+        postPatch = ''
+          ${postPatch ? ""}
+          # Move path for vgpuConfig.xml into /etc
+          sed -i 's|/usr/share/nvidia/vgpu|/etc/nvidia-vgpu-xxxxx|' nvidia-vgpud
 
-        substituteInPlace sriov-manage \
-          --replace lspci ${pkgs.pciutils}/bin/lspci \
-          --replace setpci ${pkgs.pciutils}/bin/setpci
-      '';
+          substituteInPlace sriov-manage \
+            --replace lspci ${pkgs.pciutils}/bin/lspci \
+            --replace setpci ${pkgs.pciutils}/bin/setpci
+        '';
 
-      # HACK: Using preFixup instead of postInstall since nvidia-x11 builder.sh doesn't support hooks
-      preFixup = preFixup + ''
-        for i in libnvidia-vgpu.so.${vgpuVersion} libnvidia-vgxcfg.so.${vgpuVersion}; do
-          install -Dm755 "$i" "$out/lib/$i"
-        done
-        patchelf --set-rpath ${pkgs.stdenv.cc.cc.lib}/lib $out/lib/libnvidia-vgpu.so.${vgpuVersion}
-        install -Dm644 vgpuConfig.xml $out/vgpuConfig.xml
+        # HACK: Using preFixup instead of postInstall since nvidia-x11 builder.sh doesn't support hooks
+        preFixup = ''
+          ${preFixup ? ""}
+          for i in libnvidia-vgpu.so.${vgpuVersion} libnvidia-vgxcfg.so.${vgpuVersion}; do
+            install -Dm755 "$i" "$out/lib/$i"
+          done
+          patchelf --set-rpath ${pkgs.stdenv.cc.cc.lib}/lib $out/lib/libnvidia-vgpu.so.${vgpuVersion}
+          install -Dm644 vgpuConfig.xml $out/vgpuConfig.xml
 
-        for i in nvidia-vgpud nvidia-vgpu-mgr; do
-          install -Dm755 "$i" "$bin/bin/$i"
-          # stdenv.cc.cc.lib is for libstdc++.so needed by nvidia-vgpud
-          patchelf --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-            --set-rpath $out/lib "$bin/bin/$i"
-        done
-        install -Dm755 sriov-manage $bin/bin/sriov-manage
-      '';
-    });
+          for i in nvidia-vgpud nvidia-vgpu-mgr; do
+            install -Dm755 "$i" "$bin/bin/$i"
+            # stdenv.cc.cc.lib is for libstdc++.so needed by nvidia-vgpud
+            patchelf --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+              --set-rpath $out/lib "$bin/bin/$i"
+          done
+          install -Dm755 sriov-manage $bin/bin/sriov-manage
+        '';
+      }
+    );
 
     systemd.services.nvidia-vgpud = {
       description = "NVIDIA vGPU Daemon";
-      wants = [ "syslog.target" ];
-      wantedBy = [ "multi-user.target" ];
+      wants = ["syslog.target"];
+      wantedBy = ["multi-user.target"];
 
       serviceConfig = {
         Type = "forking";
         ExecStart = "${lib.optionalString cfg.unlock.enable "${vgpu_unlock}/bin/vgpu_unlock "}${lib.getBin config.hardware.nvidia.package}/bin/nvidia-vgpud";
         ExecStopPost = "${pkgs.coreutils}/bin/rm -rf /var/run/nvidia-vgpud";
-        Environment = [ "__RM_NO_VERSION_CHECK=1" ]; # Avoids issue with API version incompatibility when merging host/client drivers
+        Environment = ["__RM_NO_VERSION_CHECK=1"]; # Avoids issue with API version incompatibility when merging host/client drivers
       };
     };
 
     systemd.services.nvidia-vgpu-mgr = {
       description = "NVIDIA vGPU Manager Daemon";
-      wants = [ "syslog.target" ];
-      wantedBy = [ "multi-user.target" ];
+      wants = ["syslog.target"];
+      wantedBy = ["multi-user.target"];
 
       serviceConfig = {
         Type = "forking";
         KillMode = "process";
         ExecStart = "${lib.optionalString cfg.unlock.enable "${vgpu_unlock}/bin/vgpu_unlock "}${lib.getBin config.hardware.nvidia.package}/bin/nvidia-vgpu-mgr";
         ExecStopPost = "${pkgs.coreutils}/bin/rm -rf /var/run/nvidia-vgpu-mgr";
-        Environment = [ "__RM_NO_VERSION_CHECK=1" ];
+        Environment = ["__RM_NO_VERSION_CHECK=1"];
       };
     };
 
     environment.etc."nvidia-vgpu-xxxxx/vgpuConfig.xml".source = config.hardware.nvidia.package + /vgpuConfig.xml;
 
-    boot.kernelModules = [ "nvidia-vgpu-vfio" ];
+    boot.kernelModules = ["nvidia-vgpu-vfio"];
 
-    environment.systemPackages = [ mdevctl ];
-    services.udev.packages = [ mdevctl ];
+    environment.systemPackages = [mdevctl];
+    services.udev.packages = [mdevctl];
   };
 }
