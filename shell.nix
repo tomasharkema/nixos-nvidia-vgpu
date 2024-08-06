a30d370e054e076d4f3d4b613a898f37b18946af
CollinDewey <collin@dewey.net>
2024-08-05 23:28:13 -0400

    Remove unused stuff

--- shell.nix
+++ shell.nix
@@ -1,7 +1,6 @@
 with import <nixpkgs> {};
 
 # Tools to run the https://github.com/VGPU-Community-Drivers/vGPU-Unlock-patcher repo
-# doesnt have mscompress (https://github.com/stapelberg/mscompress) tho :( 
 
 pkgs.mkShell {
   nativeBuildInputs = [
